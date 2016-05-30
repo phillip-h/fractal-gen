68105beaee59292afafb6fbead8232d4a10df9a1
Phillip Heikoop <phillip-h@users.noreply.github.com>
2016-05-30 16:45:10 -0600

    add color parsing

--- fractalgen.cpp
+++ fractalgen.cpp
@@ -32,13 +32,43 @@ void set_pixel(unsigned char r, unsigned char g, unsigned char b,
     vec.at(index + 3) = 0xff;
 }
 
+unsigned char htoi(std::string str)
+{
+    if (str.size() != 2)
+        return 0;
+    unsigned char res = 0;
+    unsigned char c = str.at(0);
+    if (c >= '0' && c <= '9')
+        res += (c - 0x30) * 16;
+    else if (c >= 'a' && c <= 'f')
+        res += (c - 'a' + 10) * 16;
+    else if (c >= 'A' && c <= 'F')
+        res += (c - 'A' + 10) * 16;
+    else
+        cout << "unknown hex char '" << c << "'.\n";
+
+    c = str.at(1);
+    if (c >= '0' && c <= '9')
+        res += (c - 0x30) * 1;
+    else if (c >= 'a' && c <= 'f')
+        res += (c - 'a' + 10) * 1;
+    else if (c >= 'A' && c <= 'F')
+        res += (c - 'A' + 10) * 1;
+    else
+        cout << "unknown hex char '" << c << "'.\n";
+
+    return res;
+}
+
 int main(int argc, char** argv)
 {
     if (argc < 4) {
         cout << "too few arguments!\n";
-        cout << "usage: mandelbrot <width> <height> <scale>\n";
-        cout << "                  ";
-        cout << "[cr + ci] [color depth] [offx] [offy]\n";
+        cout << "usage: fractal-gen <width> <height> <scale>\n";
+        cout << "                   ";
+        cout << "[color A] [color B] [color C]\n";
+        cout << "                   ";
+        cout << "[cr+ci] [color depth] [offx] [offy]\n";
         return -1;
     }
     
@@ -52,11 +82,83 @@ int main(int argc, char** argv)
     complex c_arg(0, 0);
     auto file = "fractal.png";
 
-    if (argc > 4 && std::string(argv[4]) != "mandelbrot") {
+    auto A_R = 0;
+    auto A_G = 0;
+    auto A_B = 0;
+
+    auto B_R = 255;
+    auto B_G = 255;
+    auto B_B = 255;
+
+    auto D_R = 0;
+    auto D_G = 0;
+    auto D_B = 0;
+
+    if (argc > 4) {
+        std::string str(argv[4]);
+        if (str.at(0) == '#')
+            str = str.substr(1, str.size() - 1);
+        if (str.size() != 6) {
+            cout << "error -- invalid color!\n";
+            cout << "express colors as '#RRGGBB'.\n";
+            return -1;
+        }
+
+        std::string sub_str = str.substr(0, 2);
+        A_R = htoi(sub_str);
+
+        sub_str = str.substr(2, 2);
+        A_G = htoi(sub_str);
+        
+        sub_str = str.substr(4, 2);
+        A_B = htoi(sub_str);
+    }
+
+    if (argc > 5) {
+        std::string str(argv[5]);
+        if (str.at(0) == '#')
+            str = str.substr(1, str.size() - 1);
+        if (str.size() != 6) {
+            cout << "error -- invalid color!\n";
+            cout << "express colors as '#RRGGBB'.\n";
+            return -1;
+        }
+
+        std::string sub_str = str.substr(0, 2);
+        B_R = htoi(sub_str);
+
+        sub_str = str.substr(2, 2);
+        B_G = htoi(sub_str);
+        
+        sub_str = str.substr(4, 2);
+        B_B = htoi(sub_str);
+    }
+    
+    if (argc > 6) {
+        std::string str(argv[6]);
+        if (str.at(0) == '#')
+            str = str.substr(1, str.size() - 1);
+        if (str.size() != 6) {
+            cout << "error -- invalid color!\n";
+            cout << "express colors as '#RRGGBB'.\n";
+            return -1;
+        }
+
+        std::string sub_str = str.substr(0, 2);
+        D_R = htoi(sub_str);
+
+        sub_str = str.substr(2, 2);
+        D_G = htoi(sub_str);
+        
+        sub_str = str.substr(4, 2);
+        D_B = htoi(sub_str);
+    }
+
+    if (argc > 7 && std::string(argv[7]) != "mandelbrot") {
         mandel = false;
         offx = 0;
         offy = 0;
-        std::string str(argv[4]);
+        std::string str(argv[7]);
         unsigned pos = 0;
         for (unsigned i = 0; i < str.size(); i++)
         {
@@ -66,7 +168,7 @@ int main(int argc, char** argv)
             }
         }
         if (pos == 0) {
-            cout << "invalid complex value!\n";
+            cout << "invalid complex argument!\n";
             return -1;
         }
         double real = atof(str.substr(0, pos - 1).c_str());
@@ -74,14 +176,38 @@ int main(int argc, char** argv)
         c_arg = complex(real, imag);
     }
 
-    if (argc > 5)
-        color_depth = atof(argv[5]);
-    if (argc > 6)
-        offx = atof(argv[6]);
-    if (argc > 7)
-        offy = atof(argv[7]);
     if (argc > 8)
+        color_depth = atof(argv[8]);
+    if (argc > 9)
+        offx = atof(argv[9]);
+    if (argc > 10)
+        offy = atof(argv[10]);
+    if (argc > 11)
         cout << "warning -- extra arguments, ignoring them.\n";
+
+    cout << "fractal information:\n\n";
+    
+    if (mandel) {
+        cout << "fractal type -- mandelbrot\n";
+    } else {
+        cout << "fractal type -- julia\n";
+        cout << "complex argument -- " << c_arg.to_str() << "\n";
+    }
+
+    cout << "\n";
+    cout << "width       -- " << width << "\n";
+    cout << "height      -- " << height << "\n";
+    cout << "pixels      -- " << (width * height * 4) << "\n";
+    cout << "scale       -- " << size << "\n";
+    cout << "color A     -- (";
+    cout << A_R << ", " << A_G << ", " << A_B << ")\n";
+    cout << "color B     -- (";
+    cout << B_R << ", " << B_G << ", " << B_B << ")\n";
+    cout << "color C     -- (";
+    cout << D_R << ", " << D_G << ", " << D_B << ")\n";
+    cout << "color depth -- " << color_depth << "\n";
+    cout << "offx        -- " << offx << "\n";
+    cout << "offy        -- " << offy << "\n";
 
     vector<unsigned char> image(width * height * 4);
 
@@ -92,18 +218,6 @@ int main(int argc, char** argv)
         double m = (y1 - y0) / (x1 - x0);
         return y0 + t * m;
     };
-
-    const auto A_R = 67;
-    const auto A_G = 10;
-    const auto A_B = 42;
-
-    const auto B_R = 232;
-    const auto B_G = 56;
-    const auto B_B = 56;
-
-    const auto D_R = 21;
-    const auto D_G = 21;
-    const auto D_B = 21;
 
     auto comp_color_r = [&] (unsigned val)
     {
